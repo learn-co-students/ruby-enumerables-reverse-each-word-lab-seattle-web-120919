05b32b4051ad9c878f8bb8f70b88670cf3fdd91a
kkirby16 <kkirby16@my.whitworth.edu>
2020-01-09 14:19:00 -0800

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,16 @@
 require 'pp'
 require 'pry'
 
-def reverse_each_word(sentence)
-  reversed_words = []
-  sentence.split.each do |word|
-    reversed_words << word.reverse
-  end
-  reversed_words.join(" ")
-end
+# def reverse_each_word(sentence) #how to do this using .each
+#   split_sentence = sentence.split(" ")
+#   array = []
+#  split_sentence.each do |word| 
+#   array.push(word.reverse) #can put a method inside another method
+#   end 
+#   array.join(" ") #converts from array type to string and gives option of how you want to join them. 
+#   end 
 
-def reverse_each_word(sentence) #using collect it can  be done #using one line as shown below.
-  sentence.split.collect {|word| word.reverse}.join(" ")
+def reverse_each_word(sentence)
+  separated_sentence = sentence.split(" ")
+  separated_sentence.collect { |word| word.reverse}.join(" ") #.join takes in an argument of what you want to join the array elements by... whether that is a space or a dash or whatever. 
 end 
-  
